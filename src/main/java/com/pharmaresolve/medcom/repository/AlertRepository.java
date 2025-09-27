eba4b06ff077b65364f0897d635b3cf3d0f15f3a
oussama benberkane <oussamabenberkane.pro@gmail.com>
2025-09-27 14:01:14 +0200

    feat(repository): add query methods for watchlist alerts and mailjet tracking

--- src/main/java/com/pharmaresolve/medcom/repository/AlertRepository.java
+++ src/main/java/com/pharmaresolve/medcom/repository/AlertRepository.java
@@ -1,6 +1,8 @@
 package com.pharmaresolve.medcom.repository;
 
 import com.pharmaresolve.medcom.domain.Alert;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,9 @@ import org.springframework.stereotype.Repository;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface AlertRepository extends JpaRepository<Alert, Long> {}
+public interface AlertRepository extends JpaRepository<Alert, Long> {
+
+    Page<Alert> findByWatchlistItemWatchlistIdOrderByCreatedDesc(Long watchlistId, Pageable pageable);
+
+    Alert findByMailjetMessageId(String mailjetMessageId);
+}
